31ea87b02be1f292eeb57865e1ae416cb3bd5b7b
Kayobe Automation <kayobe-automation@example.org>
2021-07-05 12:24:30 +0100

    Don't configure public network on external interface
    I originally did this so that I could run tempest from outside of the VM and I
    needed the service catalog to use the external IP, but that plan had a floor...
    You still need to be able to access VMs. Instead, I've switched to running
    tempest via ansible so that I can run it one the remote host. Using an
    isolated bridge has the advantage that the tempest_openrc will remain unchanged
    between all in ones so that we don't need a different openrc file per
    environment (the public IP is fixed).

--- terraform/scripts/configure-local-networking.sh
+++ terraform/scripts/configure-local-networking.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# IP of the seed hypervisor on the OpenStack 'public' network created by init-runonce.sh.
+public_ip="10.0.2.1"
+
 sudo ip l add breth1 type bridge
 sudo ip l set breth1 up
 sudo ip a add 192.168.33.3/24 dev breth1
@@ -7,3 +10,23 @@ sudo ip l add eth1 type dummy
 sudo ip l set eth1 up
 sudo ip l set eth1 master breth1
 
+iface=$(ip route | awk '$1 == "default" {print $5; exit}')
+
+#sudo iptables -A POSTROUTING -t nat -o $iface -j MASQUERADE
+sudo sysctl -w net.ipv4.conf.all.forwarding=1
+
+# Configure port forwarding from the hypervisor to the Horizon GUI on the
+# controller.
+sudo iptables -A FORWARD -i $iface -o breth1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A FORWARD -i breth1 -o $iface -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+for port in $forwarded_ports; do
+  # Allow new connections.
+  sudo iptables -A FORWARD -i $iface -o breth1 -p tcp --syn --dport $port -m conntrack --ctstate NEW -j ACCEPT
+  # Destination NAT.
+  sudo iptables -t nat -A PREROUTING -i $iface -p tcp --dport $port -j DNAT --to-destination $controller_vip
+done
+
+# Configure an IP on the 'public' network to allow access to/from the cloud.
+if ! sudo ip a show dev breth1 | grep $public_ip/24 >/dev/null 2>&1; then
+  sudo ip a add $public_ip/24 dev breth1
+fi
