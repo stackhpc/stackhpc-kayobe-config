7ef439a1a33597efa232876f1d429abb8140feb0
Will Szumski <will@stackhpc.com>
2021-12-08 14:31:57 +0000

    Deploy a local pulp server on the seed
    Co-authored-by: Michal Nasiadka <michal@stackhpc.com>
    Co-authored-by: Bartosz Bezak <bartosz@stackhpc.com>

---
+++ etc/kayobe/containers/pulp/settings.py
@@ -0,0 +1,4 @@
+CONTENT_ORIGIN='http://{{ ansible_fqdn }}'
+ANSIBLE_API_HOSTNAME='http://{{ ansible_fqdn }}'
+ANSIBLE_CONTENT_HOSTNAME='http://{{ ansible_fqdn }}/pulp/content'
+TOKEN_AUTH_DISABLED=True
