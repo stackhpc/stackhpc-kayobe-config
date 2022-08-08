30017eac0f2228c4983c0fecd2c75b8c2e591115
Mark Goddard <mark@stackhpc.com>
2022-08-08 10:49:05 +0100

    Support loadlist specific overrides
    Co-Authored-By: Will Szumski <will@stackhpc.com>

--- .automation.conf/config.sh
+++ .automation.conf/config.sh
@@ -10,8 +10,12 @@ export TEMPEST_CONCURRENCY=2
 KAYOBE_AUTOMATION_TEMPEST_CONF_OVERRIDES="${KAYOBE_AUTOMATION_CONFIG_PATH}/tempest/tempest.overrides.conf"
 
 if [ ! -z ${KAYOBE_ENVIRONMENT:+x} ]; then
-  # NOTE: could dynamically switch this based on environment
-  KAYOBE_AUTOMATION_TEMPEST_CONF_OVERRIDES="${KAYOBE_AUTOMATION_CONFIG_PATH}/tempest/tempest-${KAYOBE_ENVIRONMENT}.overrides.conf"
+  KAYOBE_AUTOMATION_TEMPEST_CONF_OVERRIDES="${KAYOBE_AUTOMATION_CONFIG_PATH}/tempest/tempest-${KAYOBE_ENVIRONMENT}-${KAYOBE_AUTOMATION_TEMPEST_LOADLIST:-}.overrides.conf"
+
+  # Check if loadlist specific overrides exist, if not fallback to environment overrides.
+  if [ ! -e "${KAYOBE_AUTOMATION_TEMPEST_CONF_OVERRIDES}" ]; then
+      KAYOBE_AUTOMATION_TEMPEST_CONF_OVERRIDES="${KAYOBE_AUTOMATION_CONFIG_PATH}/tempest/tempest-${KAYOBE_ENVIRONMENT}.overrides.conf"
+  fi
 
   if [[ "$KAYOBE_ENVIRONMENT" =~ "aio" ]]; then
     # Seem to get servers failing to spawn with higher concurrency
