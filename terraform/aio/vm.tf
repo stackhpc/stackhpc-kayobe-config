5812a90fea90af4559d6a796d2307dd3243377ee
Will Szumski <will@stackhpc.com>
2022-09-02 17:43:21 +0100

    Copy approach used for Ubuntu
    https://github.com/stackhpc/stackhpc-kayobe-config/pull/138

--- terraform/aio/vm.tf
+++ terraform/aio/vm.tf
@@ -1,7 +1,3 @@
-variable "ssh_private_key" {
-  type = string
-}
-
 variable "ssh_public_key" {
   type = string
 }
@@ -41,15 +37,9 @@ data "openstack_networking_subnet_v2" "network" {
   name = var.aio_vm_subnet
 }
 
-resource "openstack_compute_keypair_v2" "keypair" {
-  name = var.aio_vm_keypair
-  public_key = file(var.ssh_public_key)
-}
-
 resource "openstack_compute_instance_v2" "kayobe-aio" {
   name         = var.aio_vm_name
   flavor_name  = var.aio_vm_flavor
-  key_pair     = var.aio_vm_keypair
   config_drive = true
   user_data    = file("templates/userdata.cfg.tpl")
   network {
@@ -63,31 +53,5 @@ resource "openstack_compute_instance_v2" "kayobe-aio" {
     boot_index            = 0
     destination_type      = "volume"
     delete_on_termination = true
-  }
-
-  provisioner "file" {
-    source      = "scripts/configure-local-networking.sh"
-    destination = "/home/centos/configure-local-networking.sh"
-
-    connection {
-      type        = "ssh"
-      host        = self.access_ip_v4
-      user        = "centos"
-      private_key = file(var.ssh_private_key)
-    }
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo bash /home/centos/configure-local-networking.sh"
-    ]
-
-    connection {
-      type        = "ssh"
-      host        = self.access_ip_v4
-      user        = "centos"
-      private_key = file(var.ssh_private_key)
-    }
-
   }
 }
