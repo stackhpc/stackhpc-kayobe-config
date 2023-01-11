9960aa50cd3a39a7e450b4622c6bd873f69ac985
Scott Davidson <scott@stackhpc.com>
2023-01-11 14:11:12 +0000

    Point to correct kayobe config branch

--- etc/kayobe/environments/aufn-ceph/a-universe-from-nothing.sh
+++ etc/kayobe/environments/aufn-ceph/a-universe-from-nothing.sh
@@ -11,7 +11,7 @@ set -eu
 
 BASE_PATH=~
 KAYOBE_BRANCH=stackhpc/yoga
-KAYOBE_CONFIG_BRANCH=yoga-aufn
+KAYOBE_CONFIG_BRANCH=stackhpc/yoga
 KAYOBE_ENVIRONMENT=aufn-ceph
 
 PELICAN_HOST="10.0.0.34 pelican pelican.service.compute.sms-lab.cloud"
