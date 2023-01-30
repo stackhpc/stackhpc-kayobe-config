8b1a2f65a19f4772861db61b99cd1b73c2020dd0
Jack Hodgkiss <jack@stackhpc.com>
2023-01-30 15:52:10 +0000

    feat: reduce concurrency and remove load list

--- .automation.conf/config.sh
+++ .automation.conf/config.sh
@@ -3,7 +3,7 @@
 
 # See: https://github.com/stackhpc/docker-rally/blob/master/bin/rally-verify-wrapper.sh for a full list of tempest parameters that can be overriden.
 # You can override tempest parameters like so:
-export TEMPEST_CONCURRENCY=8
+export TEMPEST_CONCURRENCY=6
 # Specify single test whilst experimenting
 #export TEMPEST_PATTERN="${TEMPEST_PATTERN:-tempest.api.compute.servers.test_create_server.ServersTestJSON.test_host_name_is_same_as_server_name}"
 
@@ -28,6 +28,3 @@ fi
 if [[ -f ${KAYOBE_AUTOMATION_REPO_ROOT}/etc/kolla/public-openrc.sh ]]; then
     export TEMPEST_OPENRC="$(< ${KAYOBE_AUTOMATION_REPO_ROOT}/etc/kolla/public-openrc.sh)"
 fi
-
-export KAYOBE_AUTOMATION_TEMPEST_LOADLIST=tempest-full
-export KAYOBE_AUTOMATION_TEMPEST_SKIPLIST=tempest-full
