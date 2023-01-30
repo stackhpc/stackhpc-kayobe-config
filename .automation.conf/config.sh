141285f72644e2256d2d64e74eb705548161e4d9
Jack Hodgkiss <jack@stackhpc.com>
2023-01-30 15:52:10 +0000

    feat: revert `TEMPEST_CONCURRENCY` to two

--- .automation.conf/config.sh
+++ .automation.conf/config.sh
@@ -3,7 +3,7 @@
 
 # See: https://github.com/stackhpc/docker-rally/blob/master/bin/rally-verify-wrapper.sh for a full list of tempest parameters that can be overriden.
 # You can override tempest parameters like so:
-export TEMPEST_CONCURRENCY=6
+export TEMPEST_CONCURRENCY=2
 # Specify single test whilst experimenting
 #export TEMPEST_PATTERN="${TEMPEST_PATTERN:-tempest.api.compute.servers.test_create_server.ServersTestJSON.test_host_name_is_same_as_server_name}"
 
