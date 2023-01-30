6dc7035293d7b4b359353b34fbc238b04a7eda60
Jack Hodgkiss <jack@stackhpc.com>
2023-01-30 15:52:10 +0000

    feat: load `ci-multinode` tempest settings conditionally
    Firstly, the skip-list has been renamed from `tempest-full` to `ci-multinode` as this
    list would be environment specific. Secondly the `config.sh` script now contains a
    conditional for the `ci-multinode` environment allowing for the test list and
    concurrency settings to be loaded without impacting other environments.

--- .automation.conf/config.sh
+++ .automation.conf/config.sh
@@ -19,6 +19,12 @@ if [ ! -z ${KAYOBE_ENVIRONMENT:+x} ]; then
     # Seem to get servers failing to spawn with higher concurrency
     export TEMPEST_CONCURRENCY=1
   fi
+  if [[ "$KAYOBE_ENVIRONMENT" =~ "ci-multinode" ]]; then
+      # Increase concurrency for multinode environment. Eight has proven to high, leading to failing tests due to poor CEPH performance.
+      export TEMPEST_CONCURRENCY=6
+      export KAYOBE_AUTOMATION_TEMPEST_LOADLIST=tempest-full
+      export KAYOBE_AUTOMATION_TEMPEST_SKIPLIST=ci-multinode
+  fi
 fi
 
 if [[ -z "${KAYOBE_AUTOMATION_TEMPEST_CONF_OVERRIDES:+x}" ]] || [[ ! -e "${KAYOBE_AUTOMATION_TEMPEST_CONF_OVERRIDES}" ]]; then
