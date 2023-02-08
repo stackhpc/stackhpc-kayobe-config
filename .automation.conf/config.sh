54c8d2835f2fd71faf7072030a0f8909cc3198f3
Will Szumski <will@stackhpc.com>
2023-02-08 12:08:12 +0000

    Reduce tempest concurrency to 1

--- .automation.conf/config.sh
+++ .automation.conf/config.sh
@@ -23,7 +23,7 @@ if [ ! -z ${KAYOBE_ENVIRONMENT:+x} ]; then
   if [[ "$KAYOBE_ENVIRONMENT" =~ "ci-multinode" ]]; then
     # SMSLab is currently running with 1G switches. This causes tests using volumes and images to fail if
     # the concurrency is set too high.
-    export TEMPEST_CONCURRENCY=2
+    export TEMPEST_CONCURRENCY=1
     export KAYOBE_AUTOMATION_TEMPEST_LOADLIST=tempest-full
     export KAYOBE_AUTOMATION_TEMPEST_SKIPLIST=ci-multinode
   fi
