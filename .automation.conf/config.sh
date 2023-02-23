45c06f9eb5cc0d4661e1231a42a2d4a7f4bf8bf4
Jack Hodgkiss <identity@jackhodgkiss.uk>
2023-02-23 11:49:23 +0000

    feat: leave `TEMPEST_OPENRC` to `config.sh`

--- .automation.conf/config.sh
+++ .automation.conf/config.sh
@@ -25,9 +25,9 @@ if [[ -z "${KAYOBE_AUTOMATION_TEMPEST_CONF_OVERRIDES:+x}" ]] || [[ ! -e "${KAYOB
     KAYOBE_AUTOMATION_TEMPEST_CONF_OVERRIDES="${KAYOBE_AUTOMATION_CONFIG_PATH}/tempest/tempest.overrides.conf"
 fi
 
-# if [[ -f ${KAYOBE_AUTOMATION_REPO_ROOT}/etc/kolla/public-openrc.sh ]]; then
-#     export TEMPEST_OPENRC="$(< ${KAYOBE_AUTOMATION_REPO_ROOT}/etc/kolla/public-openrc.sh)"
-# fi
+if [[ -f ${KAYOBE_AUTOMATION_REPO_ROOT}/etc/kolla/public-openrc.sh ]]; then
+    export TEMPEST_OPENRC="$(< ${KAYOBE_AUTOMATION_REPO_ROOT}/etc/kolla/public-openrc.sh)"
+fi
 
 export KAYOBE_AUTOMATION_TEMPEST_LOADLIST=tempest-full
 export KAYOBE_AUTOMATION_TEMPEST_SKIPLIST=tempest-full
