504fdb4db2d46846cbbdd6c3218deb1beb1b09f3
scrungus <tyler@stackhpc.com>
2023-07-19 14:42:47 +0100

    removed scripts

--- tools/azimuth-config-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -eux 
-
-echo source ~/venvs/azimuth/bin/activate
-echo cd ~/src/$1-azimuth-config/
-echo source ./bin/activate $1
--- tools/kayobe-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eux 
-
-source ~/venvs/kayobe/bin/activate
-cd ~/src/kayobe-config
-source kayobe-env --environment $1
-source <(kayobe complete)
-export KAYOBE_VAULT_PASSWORD=$(< ~/vault.secret)
-export KAYOBE_ENVIRONMENT=$1
