c032f6898301fe50bd5c81be4dc156c2b0a63c57
Alex-Welsh <alex@stackhpc.com>
2024-03-01 17:03:17 +0000

    Container image builds misc improvements

--- tools/scan-images.sh
+++ tools/scan-images.sh
@@ -1,17 +1,21 @@
+#!/usr/bin/env bash
 set -eo pipefail
 
 # Check correct usage
 if [[ ! $2 ]]; then
-    echo "Usage: overcloud-ubuntu-upgrade.sh <os-distribution> <image-tag>"
-    exit 2
+  echo "Usage: scan-images.sh <os-distribution> <image-tag>"
+  exit 2
 fi
 
 set -u
 
 # Check that trivy is installed
 if ! trivy --version; then
-    echo 'Please install trivy: curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin v0.49.1'
+  echo 'Please install trivy: curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin v0.49.1'
 fi
+
+# Clear any previous outputs
+rm -rf image-scan-output
 
 # Make a fresh output directory
 mkdir -p image-scan-output
@@ -21,6 +25,9 @@ docker image ls --filter "reference=ark.stackhpc.com/stackhpc-dev/$1-*:$2" > $1-
 
 # Make a file of imagename:tag
 images=$(grep --invert-match --no-filename ^REPOSITORY $1-scanned-container-images.txt | sed 's/ \+/:/g' | cut -f 1,2 -d:)
+
+# Ensure output files exist
+touch image-scan-output/clean-images.txt image-scan-output/dirty-images.txt
 
 # If Trivy detects no vulnerabilities, add the image name to clean-images.txt.
 # If there are vulnerabilities detected, add it to dirty-images.txt and
