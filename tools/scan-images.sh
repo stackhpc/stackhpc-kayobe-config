76bf0ad0cf661ffaa27c6395255ea1a13419a0cb
Jake Hutchinson <jake@stackhpc.com>
2024-03-14 12:33:06 +0000

    Support allow lists in Trivy
    (cherry picked from commit 571473788086be23836ce6d8b05ceed68d445bff)

--- tools/scan-images.sh
+++ tools/scan-images.sh
@@ -36,6 +36,19 @@ touch image-scan-output/clean-images.txt image-scan-output/dirty-images.txt imag
 # critical-images.txt
 for image in $images; do
   filename=$(basename $image | sed 's/:/\./g')
+  imagename=$(echo $filename | cut -d "." -f 1)
+  global_vulnerabilities=$(yq .global_allowed_vulnerabilities[] src/kayobe-config/etc/kayobe/trivy/allowed-vulnerabilities.yml)
+  image_vulnerabilities=$(yq .$imagename'_allowed_vulnerabilities[]' src/kayobe-config/etc/kayobe/trivy/allowed-vulnerabilities.yml)
+  rc=$?
+  touch .trivyignore
+  for vulnerability in $global_vulnerabilities; do
+    echo $vulnerability >> .trivyignore
+  done
+  for vulnerability in $image_vulnerabilities; do
+    if [ $rc -eq 0 ]; then
+        echo $vulnerability >> .trivyignore
+    fi
+  done
   if $(trivy image \
           --quiet \
           --exit-code 1 \
@@ -84,4 +97,5 @@ for image in $images; do
       echo "${image}" >> image-scan-output/dirty-images.txt
     fi
   fi
+  rm .trivyignore
 done
