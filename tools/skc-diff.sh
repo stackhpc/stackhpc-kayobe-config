df1011e41322c0c9487282be9b47b386c3c15377
Alex-Welsh <alex@stackhpc.com>
2024-07-19 13:40:41 +0100

    Begin changing to better formatting

--- tools/skc-diff.sh
+++ tools/skc-diff.sh
@@ -4,7 +4,7 @@
 # checkout and either upstream stackhpc-kayobe-config (default) or the same
 # branch 4 weeks ago (add --month argument).
 
-SKC_BRANCH=stackhpc/yoga
+SKC_BRANCH=stackhpc/2024.1
 LOCAL_BRANCH=HEAD
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
@@ -17,9 +17,12 @@ git fetch nonduplicatedremotename > /dev/null 2>&1
 
 if [ "$1" = "--month" ];
 then
+    # Top version is the "working" but bad approach, bottom version is not working yet but will be better once we fix it
     git diff "$LOCAL_BRANCH@{4 weeks ago}" $LOCAL_BRANCH -- releasenotes/notes/ | grep '^\+' | grep -v '\(+++\|---\)' | sed s/^+//g
+    # git diff --name-status "$LOCAL_BRANCH@{4 weeks ago}" $LOCAL_BRANCH -- releasenotes/notes/
 else
     git diff $LOCAL_BRANCH nonduplicatedremotename/$SKC_BRANCH -- releasenotes/notes/ | grep '^\+' | grep -v '\(+++\|---\)' | sed s/^+//g
+    # git diff --name-status $LOCAL_BRANCH nonduplicatedremotename/$SKC_BRANCH -- releasenotes/notes/
 fi;
 
 git remote rm nonduplicatedremotename
