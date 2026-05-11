b4911bf6262c0ba13be2cf9ab437f9305593ee2f
Owen Jones <owen@stackhpc.com>
2026-05-11 15:38:16 +0100

    Build Rocky Linux 10 container images
    Changes:
    * Updated workflow to build Rocky 10 Kolla container images
    * Updated ci-builder environment to enable valkey image building on
      Rocky 10 (and enable redis image building on Rocky 9/Ubuntu Noble)
    * Updated ci-builder and ci-aio environments with pulp tags for Rocky
      10 repositories
    * Added new OVN-LTS repository into pulp repository configuration
    * Added Rocky 10 repositories into Kolla configuration
    * Added new `stackhpc_enable_ovn_lts_repo` flag to Kolla configuration
      to inject OVN-LTS repository into repository sources
    * Updated Kolla build customisations to separate out Rocky 9 and 10
      customisations, and added customisations for Rocky 10
    * Added valkey images to pulp image list, and updated unbuildable
      images lists to reflect
    * Updated DNF configuration to include Rocky 10 repositories
    * Updated kolla-images.py to support Rocky 10 as a base distro
    * Added initial tag for Rocky 10 containers

--- tools/kolla-images.py
+++ tools/kolla-images.py
@@ -75,6 +75,9 @@ IMAGE_TO_CONTAINERS_EXCEPTIONS: Dict[str, List[str]] = {
     "prometheus-server": [
         "prometheus_server",
     ],
+    "valkey-server": [
+        "valkey",
+    ],
 }
 
 # Maps a container to the parent tag variable in the hierarchy.
@@ -101,6 +104,7 @@ CONTAINER_TO_PREFIX_VAR_EXCEPTIONS: Dict[str, str] = {
 # List of supported base distributions and versions.
 SUPPORTED_BASE_DISTROS = [
     "rocky-9",
+    "rocky-10",
     "ubuntu-noble",
 ]
 
