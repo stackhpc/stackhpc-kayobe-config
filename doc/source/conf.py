188e113915e26487bd4c77010e347ccd9dc37ec1
Pierre Riteau <pierre@stackhpc.com>
2026-07-16 15:56:00 +0200

    Add 2026.1 documentation
    The OpenStack upgrade page still needs a full review.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -29,8 +29,8 @@
 # -- StackHPC Kayobe configuration --------------------------------------
 # Variables to override
 
-current_series = "2025.1"
-previous_series = "2024.1"
+current_series = "2026.1"
+previous_series = "2025.1"
 branch = f"stackhpc/{current_series}"
 ceph_series = "squid"
 
