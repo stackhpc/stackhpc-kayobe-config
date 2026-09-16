da654a2f548ca772313af87f37fa20d4f79b3b49
Leonie Chamberlin-Medd <leonie@stackhpc.com>
2026-09-16 15:17:38 +0100

    Make updates to paths

--- etc/kayobe/environments/ci-gns3/gns3-deployment.sh
+++ etc/kayobe/environments/ci-gns3/gns3-deployment.sh
@@ -13,7 +13,8 @@ KAYOBE_BRANCH=master
 KAYOBE_CONFIG_REF=${KAYOBE_CONFIG_REF:-master}
 KAYOBE_ENVIRONMENT=${KAYOBE_ENVIRONMENT:-ci-gns3}
 KAYOBE_PATH=$BASE_PATH/kayobe
-KAYOBE_CONFIG_PATH=$KAYOBE_PATH/config/src/kayobe-config
+KAYOBE_CONFIG_PATH=$KAYOBE_PATH/config/src/kayobe-config/etc/kayobe
+KAYOBE_CONFIG_ROOT=$KAYOBE_PATH/config/src/kayobe-config
 GNS3_ROLE_PATH=$BASE_PATH/gns3-ansible-role
 
 echo "STARTING DEMO SCRIPT..."
@@ -26,12 +27,12 @@ if [[ ! -d $KAYOBE_PATH ]]; then
 fi
 
 mkdir -p "$KAYOBE_PATH/config/src"
-if [[ ! -d $KAYOBE_CONFIG_PATH ]]; then
+if [[ ! -d $KAYOBE_CONFIG_ROOT ]]; then
   echo "CLONING KAYOBE CONFIG REPO..."
   git clone https://github.com/stackhpc/stackhpc-kayobe-config \
-    "$KAYOBE_CONFIG_PATH"
+    "$KAYOBE_CONFIG_ROOT"
   (
-    cd "$KAYOBE_CONFIG_PATH"
+    cd "$KAYOBE_CONFIG_ROOT"
     git checkout "$KAYOBE_CONFIG_REF"
   )
   echo "KAYOBE CONFIG REPO CLONED"
@@ -40,10 +41,16 @@ fi
 
 # Create breth1 stuff
 echo "CREATING BRIDGE AND DUMMY INTERFACES..."
-sudo ip l add breth1 type bridge
+if ! ip l show breth1 >/dev/null 2>&1; then
+  sudo ip l add breth1 type bridge
+fi
 sudo ip l set breth1 up
-sudo ip a add 192.168.33.3/24 dev breth1
-sudo ip l add dummy1 type dummy
+if ! ip a show breth1 | grep -q '192.168.33.3/24'; then
+  sudo ip a add 192.168.33.3/24 dev breth1
+fi
+if ! ip l show dummy1 >/dev/null 2>&1; then
+  sudo ip l add dummy1 type dummy
+fi
 sudo ip l set dummy1 up
 sudo ip l set dummy1 master breth1
 
@@ -61,7 +68,9 @@ echo "KAYOBE DEV ENVIRONMENT INSTALLED"
 echo "SETTING UP ENVIRONMENT..."
 source ~/kayobe-venv/bin/activate
 
-git clone https://github.com/stackhpc/ansible-role-gns3.git "$GNS3_ROLE_PATH"
+if [[ ! -d "$GNS3_ROLE_PATH" ]]; then
+  git clone https://github.com/stackhpc/ansible-role-gns3.git "$GNS3_ROLE_PATH"
+fi
 
 cd "$GNS3_ROLE_PATH"
 
@@ -70,17 +79,20 @@ echo "RUNNING ANSIBLE PLAYBOOK TO INSTALL GNS3..."
 ansible-playbook -i inventory.ini trialplaybook.yml -vvv
 echo "GNS3 INSTALLED!"
 
-mkdir -p $KAYOBE_CONFIG_PATH/inventory/host_vars/
+mkdir -p "$KAYOBE_CONFIG_PATH/inventory/host_vars/"
+
 # copy host_vars for switch to kayobe config
 sudo cp "$GNS3_ROLE_PATH/roles/gns3/files/switch1" \
-  "$KAYOBE_CONFIG_PATH/etc/kayobe/inventory/host_vars/switch1"
+  "$KAYOBE_CONFIG_PATH/inventory/host_vars/switch1"
 
 echo "SWITCH HOST_VARS COPIED"
 
 cd "$KAYOBE_PATH"
 
-source "$KAYOBE_CONFIG_PATH/kayobe-env" --environment "$KAYOBE_ENVIRONMENT"
-pip install -e "$KAYOBE_PATH"
+# Activate environment
+pushd "$KAYOBE_CONFIG_ROOT"
+source kayobe-env --environment "$KAYOBE_ENVIRONMENT"
+
 
 kayobe control host bootstrap
 kayobe physical network configure --group mgmt-switches
